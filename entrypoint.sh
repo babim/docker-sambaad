7036a130f898415b3529a1e49d0b61bd5a4fa05c
Duc Anh Babim <ducanh.babim@yahoo.com>
2017-01-01 15:52:55 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,9 +20,6 @@ appSetup () {
 SAMBA_PASSWORD=${SAMBA_PASSWORD:-`(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c20; echo) 2>/dev/null`}
 echo "Samba password set to: $SAMBA_PASSWORD"
 
-# Fix nameserver
-echo -e "search ${SAMBA_REALM}\nnameserver 127.0.0.1" > /etc/resolv.conf
-
 # Provision domain
 rm -f /etc/samba/smb.conf
 rm -rf /var/lib/samba/*
@@ -65,6 +62,10 @@ touch "${SETUP_LOCK_FILE}"
 }
 
 appStart () {
+# Fix nameserver
+echo -e "search ${SAMBA_REALM}\nnameserver 127.0.0.1" > /etc/resolv.conf
+echo -e "127.0.0.1 $HOSTNAME" > /etc/hosts
+echo -e "$HOSTNAME" > /etc/hostname
     # setup
     if [ ! -f "${SETUP_LOCK_FILE}" ]; then
       appSetup
